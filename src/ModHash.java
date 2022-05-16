c058f66636e5168b4dccafb878dc01b83d84afcc
Jonathan <jhyahav2@gmail.com>
2022-05-16 12:55:16 +0300

    modhash

--- src/ModHash.java
+++ src/ModHash.java
@@ -1,13 +1,28 @@
+import java.util.Random;
 
 public class ModHash {
-	
+
+	private final int m;
+	private final long p;
+	private final int a;
+	private final int b;
+
+	public ModHash(int a, int b, long p, int m) {
+		this.a = a;
+		this.b = b;
+		this.p = p;
+		this.m = m;
+	}
+
 	public static ModHash GetFunc(int m, long p){
-		// TODO: implement random choice of mod function from family
-		return null;
+		Random random = new Random();
+		int a = random.nextInt((int) p) + 1; // ensure a >= 1
+		int b = random.nextInt((int) p);
+
+		return new ModHash(a, b, p, m);
 	}
 	
 	public int Hash(long key) {
-		// TODO implement hash function
-		return 0;
+		return (int) (((this.a * key + this.b) % this.p) % this.m);
 	}
 }
